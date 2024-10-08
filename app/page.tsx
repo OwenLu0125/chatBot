73a57e7d8241377a0c1c085fac32ad570bfe9d4e
OwenLu0125 <121356760+OwenLu0125@users.noreply.github.com>
2024-10-09 02:23:28 +0800

    Panel layout adjust

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home()  {
       {/* Slide-out chat panel */}
       <div
         ref={chatRef}
-        className={`fixed top-0 right-0 w-3/4 md:w-1/3  h-full bg-white shadow-lg transform transition-transform duration-300 ease-in-out ${isChatOpen ? 'translate-x-0' : 'translate-x-full'}`}
+        className={`fixed top-0 right-0 w-screen md:w-1/3  h-full bg-white shadow-lg transform transition-transform duration-300 ease-in-out ${isChatOpen ? 'translate-x-0' : 'translate-x-full'}`}
       >
         <div className="h-full p-5">
           <RwdChat />
